27abb0450dd177c612cacce117d9784121ed2f8a
sindlinger <candeia.goncalves@gmail.com>
2024-11-05 02:19:26 -0300

    depois de iniciar os 3 servicos, back, front e docker

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,  // Ativa o modo estrito para debugging de erros comuns
+  // Remova 'swcMinify' se não suportada ou necessária 
 };
 
 export default nextConfig;
